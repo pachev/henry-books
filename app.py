9f4d168942454663ce5d2eb2f1c67698726aa88c
Pachev Joseph <pachevjoseph@gmail.com>
2016-11-21 15:26:17 -0500

    Adds ability to connect to mysql and display

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flaskext.mysql import MySQL
+
+# General Settings
 app = Flask(__name__)
 
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'assignment_3'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
-@app.route("/")
+mysql = MySQL()
+mysql.init_app(app)
+
+
+# Routes to index page
+@app.route('/')
 def main():
     return render_template('index.html')
 
+@app.route('/data')
+def showData():
+    # MySQL connector
+    conn = mysql.connect()
+
+    cursor = conn.cursor()
+
+    cursor.execute("select * from book")
+
+    data = cursor.fetchall();
+    return jsonify(data)
+
+# This allows app to run with standar python commnand
 if __name__ == "__main__":
     app.run()
