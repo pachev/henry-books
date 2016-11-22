aacaaf8b03839e64521adfb540f24741ec03274e
Pachev Joseph <pachevjoseph@gmail.com>
2016-11-21 20:15:42 -0500

    Adds routing for all templates

--- app.py
+++ app.py
@@ -12,23 +12,43 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql = MySQL()
 mysql.init_app(app)
 
+# MySQL connector
+conn = mysql.connect()
+
+cursor = conn.cursor()
 
 # Routes to index page
 @app.route('/')
 def main():
-    return render_template('index.html')
-
-@app.route('/data')
-def showData():
-    # MySQL connector
-    conn = mysql.connect()
-
-    cursor = conn.cursor()
 
     cursor.execute("select * from book")
 
     data = cursor.fetchall();
-    return jsonify(data)
+    return render_template('index.html', data=data)
+
+@app.route('/author')
+def author():
+
+    cursor.execute("select * from author")
+
+    data = cursor.fetchall();
+    return render_template('author.html', data=data)
+
+@app.route('/copy')
+def copy():
+
+    cursor.execute("select * from copy")
+
+    data = cursor.fetchall();
+    return render_template('copy.html', data=data)
+
+@app.route('/publisher')
+def publisher():
+
+    cursor.execute("select * from publisher")
+
+    data = cursor.fetchall();
+    return render_template('publisher.html', data=data)
 
 # This allows app to run with standar python commnand
 if __name__ == "__main__":
