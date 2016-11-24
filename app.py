5654c5c420ffe3eff1d08faf4d72e0dcb04eaef3
Alan Kopetman <akope001@fiu.edu>
2016-11-24 16:53:13 -0500

    Added search and dynamic table column population for queries

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flaskext.mysql import MySQL
 
 # General Settings
@@ -18,13 +18,23 @@ conn = mysql.connect()
 
 cursor = conn.cursor()
 
+#Give context for tables
+def get_table_context(cursor, cursor_data):
+    ''' returns name of columns from query in data[0]
+        takes a cursor and data from a fetchall()
+    '''
+    cursor_data = list(cursor_data)
+    cursor_data.insert(0,tuple([i[0] for i in cursor.description]))
+    data = tuple(cursor_data)
+    return data
+
 # Routes to index page
 @app.route('/')
 def main():
 
     cursor.execute("select * from book")
 
-    data = cursor.fetchall();
+    data = cursor.fetchall()
     return render_template('index.html', data=data)
 
 @app.route('/author')
@@ -32,7 +42,7 @@ def author():
 
     cursor.execute("select * from author")
 
-    data = cursor.fetchall();
+    data = cursor.fetchall()
     return render_template('author.html', data=data)
 
 @app.route('/copy')
@@ -40,7 +50,7 @@ def copy():
 
     cursor.execute("select * from copy")
 
-    data = cursor.fetchall();
+    data = cursor.fetchall()
     return render_template('copy.html', data=data)
 
 @app.route('/publisher')
@@ -48,8 +58,18 @@ def publisher():
 
     cursor.execute("select * from publisher")
 
-    data = cursor.fetchall();
+    data = cursor.fetchall()
     return render_template('publisher.html', data=data)
+
+@app.route('/search', methods=['GET', 'POST'])
+def search():
+    print(request.form['search'])
+    if request.method == "POST":
+        cursor.execute("{}".format(request.form['search']))
+        cursor_data = cursor.fetchall()
+        data = get_table_context(cursor, cursor_data)
+
+    return render_template('search.html', data=data)
 
 # This allows app to run with standar python commnand
 if __name__ == "__main__":
