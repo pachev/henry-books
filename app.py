0404a3f361ef84bcd7c1e92c77bea03c4ced557f
Alan Kopetman <akope001@fiu.edu>
2016-11-24 10:44:01 -0500

    refactored and modularized templates. CSS served from static and child layouts extend from layout

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_DB'] = 'assignment_3'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'awolin82'
 
 mysql = MySQL()
 mysql.init_app(app)
