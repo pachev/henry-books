b59ca9d8e10b7fa4d211dc1c54cd46c1310291e5
Pachev Joseph <pachevjoseph@gmail.com>
2016-11-25 10:32:21 -0600

    Adds update modal with minimal functionality

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from flaskext.mysql import MySQL
 
 # General Settings
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
@@ -18,12 +18,18 @@ conn = mysql.connect()
 cursor = conn.cursor()
 
 # Routes to index page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
 
     cursor.execute("select * from book")
-
     data = cursor.fetchall();
+
+    # # Read the posted values
+    # _title = request.form['title']
+    # _bookcode = request.form['bookCode']
+    # _publisherCode = request.form['publisherCode']
+    # _type = request.form['type']
+    # _paperback = request.form['paperback']
     return render_template('index.html', data=data)
 
 @app.route('/author')
@@ -49,6 +55,23 @@ def publisher():
 
     data = cursor.fetchall();
     return render_template('publisher.html', data=data)
+
+@app.route('/editbook', methods=['POST'])
+def editBook():
+    _title = request.form['editTitle']
+    _bookcode = request.form['editBookcode']
+    _publishercode = request.form['editPublishercode']
+    _type = request.form['editType']
+    _paperback = request.form['editPaperback']
+
+    cursor.execute("""
+        UPDATE book SET bookcode=%s, title=%s, publishercode=%s, 
+        type=%s, paperback=%s WHERE bookcode=%s
+    """,(_bookcode, _title, _publishercode, _type, _paperback, _bookcode))
+    
+    conn.commit()
+
+    return(_title)
 
 # This allows app to run with standar python commnand
 if __name__ == "__main__":
---
+++ static/js/script.js
@@ -0,0 +1,18 @@
+$(function(){
+	$('#btnUpdate').click(function(){
+        console.log("hello")
+		
+		$.ajax({
+			url: '/editbook',
+			data: $('form').serialize(),
+			type: 'POST',
+			success: function(response){
+				console.log(response);
+                $('#editalert').show()
+			},
+			error: function(error){
+				console.log(error);
+			}
+		});
+	});
+});
